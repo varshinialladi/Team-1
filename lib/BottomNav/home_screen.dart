6fadf0247772fcd543972dc23af527e14bc9830b
VUTLA JYOTHI <23211a12h3@bvrit.ac.in>
2025-03-12 22:55:04 +0530

    This is Jyothi

--- lib/BottomNav/home_screen.dart
+++ lib/BottomNav/home_screen.dart
@@ -10,6 +10,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+
+
+      //jyothi screen 
+    );
   }
 }
